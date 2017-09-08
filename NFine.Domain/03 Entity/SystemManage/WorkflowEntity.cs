e50503fa08de71e8bc49f8a484a8b59120caf736
zc <Jay@DESKTOP-D9812FV>
2017-09-08 14:47:20 +0800

    添加项目文件。

---
+++ NFine.Domain/03 Entity/SystemManage/WorkflowEntity.cs
@@ -0,0 +1,28 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 NFine.Framework 版权所有
+ * Author: NFine
+ * Description: NFine快速开发平台
+ * Website：http://www.nfine.cn
+*********************************************************************************/
+using System;
+
+namespace NFine.Domain.Entity.SystemManage
+{
+    public class WorkflowEntity : IEntity<WorkflowEntity>, ICreationAudited, IDeleteAudited, IModificationAudited
+    {
+        public string F_Id { get; set; }
+        public string F_WorkflowName { get; set; }
+        public int? F_WorkflowType { get; set; }
+       
+        public int? F_SortCode { get; set; }
+        public bool? F_DeleteMark { get; set; }
+        public bool? F_EnabledMark { get; set; }
+        public string F_Description { get; set; }
+        public DateTime? F_CreatorTime { get; set; }
+        public string F_CreatorUserId { get; set; }
+        public DateTime? F_LastModifyTime { get; set; }
+        public string F_LastModifyUserId { get; set; }
+        public DateTime? F_DeleteTime { get; set; }
+        public string F_DeleteUserId { get; set; }
+    }
+}
